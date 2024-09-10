78ff6d817772ce7014d263a238f0a38d7331bc70
202080112 <202080112@PO-24852>
2024-09-10 11:28:19 -0400

    etape 2 done

--- JeuGladiateurs/src/personnages/Personnage.java
+++ JeuGladiateurs/src/personnages/Personnage.java
@@ -6,7 +6,11 @@ public class Personnage {
     // **************************************************************************
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Attributs">
-    // TODO : Mettre vos attributs ici
+   public String nomPersonnage;
+   public int ptsDeVie;
+   public int valeurMaxAttaque;
+   public int valeurDefense;
+   public int initiative;
     // </editor-fold>
 
     // **************************************************************************
@@ -14,11 +18,19 @@ public class Personnage {
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Constructeurs et initialisation">
     public Personnage(String nom, int attaqueMax, int defense, int pvs, int ini) {
-        // TODO : Constructeur AVEC paramÃ¨tres
+        nomPersonnage = nom;
+        valeurMaxAttaque = attaqueMax;
+        valeurDefense = defense;
+        ptsDeVie = pvs;
+        initiative = ini;
     }
 
     public Personnage() {
-        // TODO : Constructeur SANS paramÃ¨tres qui initialise Ã  ZÃ‰RO ou Ã  ""
+       nomPersonnage = "";
+        valeurMaxAttaque = 0;
+        valeurDefense = 0;
+        ptsDeVie = 0;
+        initiative = 0;
     }
     // </editor-fold>
 
@@ -26,32 +38,83 @@ public class Personnage {
     // **************************************************************************
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Getters et setters">
-    // TODO : Les getters
-    // TODO : Les setters
+    public String getNomPersonnage() {
+        return nomPersonnage;
+    }
+
+    public int getPtsDeVie() {
+        return ptsDeVie;
+    }
+
+    public int getValeurMaxAttaque() {
+        return valeurMaxAttaque;
+    }
+
+    public int getValeurDefense() {
+        return valeurDefense;
+    }
+
+    public int getInitiative() {
+        return initiative;
+    }
+    
+    public void setNomPersonnage(String nomPersonnage) {
+        this.nomPersonnage = nomPersonnage;
+    }
+
+    public void setPtsDeVie(int ptsDeVie) {
+        this.ptsDeVie = ptsDeVie;
+    }
+
+    public void setValeurMaxAttaque(int valeurMaxAttaque) {
+        this.valeurMaxAttaque = valeurMaxAttaque;
+    }
+
+    public void setValeurDefense(int valeurDefense) {
+        this.valeurDefense = valeurDefense;
+    }
+
+    public void setInitiative(int initiative) {
+        this.initiative = initiative;
+    }
     // </editor-fold>
 
     // **************************************************************************
     // **************************************************************************
     // **************************************************************************
-    // <editor-fold defaultstate="collapsed" desc="MÃ©canique de jeu">
+    // <editor-fold defaultstate="collapsed" desc="Mécanique de jeu">
     public void afficherInfosPersonnage() {
-        // TODO : Afficher les infos du personnage, tel que montrÃ© dans l'Ã©noncÃ©
+        System.out.println("\n");
+        System.out.println(nomPersonnage);
+        System.out.println("Attaque : " + valeurMaxAttaque);
+        System.out.println("Défense : " + valeurDefense);
+        System.out.println("Points de vie : " + ptsDeVie);
+        System.out.println("Initiative : " + initiative);
+        if (ptsDeVie > 0 ) {
+              System.out.println("Statut : Vivant " );
+        }
+        else 
+        {
+               System.out.println("Statut : Mort" );  
+        }
     }
 
     private int attaqueCalcul() {
         // TODO : Retourner la valeur de l'attaque du personnage.
-        // Cette valeur est trouvÃ©e alÃ©atoirement et doit se situer entre ZÃ‰RO et valeurMaxAttaque.
+        // Cette valeur est trouvée aléatoirement et doit se situer entre ZÉRO et valeurMaxAttaque.
         return 0;
     }
 
     public void frapperPersonnage(Personnage personnageCible) {
-        // TODO : RÃ©cupÃ©rer la valeur d'attaque pour ce tour, calculer les dÃ©gats,
-        //modifier les points de vie du personnage cible, afficher les dÃ©tails
-        // sur l'attaque, tel que montrÃ© dans l'Ã©noncÃ©.
+        // TODO : Récupérer la valeur d'attaque pour ce tour, calculer les dégats,
+        //modifier les points de vie du personnage cible, afficher les détails
+        // sur l'attaque, tel que montré dans l'énoncé.
     }
 
     public void setNewInitiativeRandom() {
-        // TODO : Modifier de faÃ§on alÃ©atoire la valeur INI du personnage.
+        // TODO : Modifier de façon aléatoire la valeur INI du personnage.
     }
     // </editor-fold>
+    
+    
 }
